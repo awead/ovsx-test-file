b76252e212cbc515ecf00a9d4030936b8834ea2b
Adam Wead <awead@users.noreply.github.com>
2025-04-19 01:31:50 -0400

    Adding language-specific configurations

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -1,7 +1,9 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
+import { executeTestFile } from '../../src/terminal';
+import * as sinon from 'sinon';
 
-describe('Extension Test Suite', function() {
+describe('Extension Test Suite', function () {
   this.timeout(10000); // 10 second timeout
   vscode.window.showInformationMessage('Start all tests.');
 
@@ -12,4 +14,17 @@ describe('Extension Test Suite', function() {
     assert.ok(commands.includes('testFile.run'));
   });
 
+  context('when there is no activeTextEditor', () => {
+    it('shows an error message', () => {
+      const spy = sinon.spy(vscode.window, 'showErrorMessage');
+      const getActiveTextEditorStub = sinon.stub(vscode.window, 'activeTextEditor').get(() => undefined);
+
+      executeTestFile({});
+
+      assert.ok(spy.calledWith('No active file to test'));
+
+      spy.restore();
+      getActiveTextEditorStub.restore();
+    });
+  });
 });
